a9990e5c08f23fdac34c915c3081842d2d1472b3
Cahenkorah <42823702+Cahenkorah@users.noreply.github.com>
2023-07-05 18:05:15 +0000

    auth routes fix

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -11,11 +11,14 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("api/user/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
-    });
+    const response = await fetch(
+      "https://takenote-server.onrender.com/api/user/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      }
+    );
 
     const json = await response.json();
 
